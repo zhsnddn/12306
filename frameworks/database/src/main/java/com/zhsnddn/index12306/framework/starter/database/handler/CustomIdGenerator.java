bf2ed8f6cf17c1d17e3492b0688bc7357dd8c069
朱玉明 <2027606134@qq.com>
2025-06-29 23:32:38 +0800

    feat: 封装数据库持久层组件

---
+++ frameworks/database/src/main/java/com/zhsnddn/index12306/framework/starter/database/handler/CustomIdGenerator.java
@@ -0,0 +1,15 @@
+package com.zhsnddn.index12306.framework.starter.database.handler;
+
+import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
+import com.zhsnddn.index12306.framework.starter.distributedid.toolkit.SnowflakeIdUtil;
+
+/**
+ * 自定义雪花算法生成器
+ */
+public class CustomIdGenerator implements IdentifierGenerator {
+
+    @Override
+    public Number nextId(Object entity) {
+        return SnowflakeIdUtil.nextId();
+    }
+}
