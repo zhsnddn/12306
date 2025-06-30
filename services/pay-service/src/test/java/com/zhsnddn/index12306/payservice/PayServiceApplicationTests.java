12beb03661ac29b66f0d35d203bea7dfdc6e73cb
朱玉明 <2027606134@qq.com>
2025-06-30 13:45:48 +0800

    feat: 初始化微服务

---
+++ services/pay-service/src/test/java/com/zhsnddn/index12306/payservice/PayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhsnddn.index12306.payservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ services/ticket-service/src/test/java/com/zhsnddn/index12306/ticketservice/TicketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhsnddn.index12306.ticketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
