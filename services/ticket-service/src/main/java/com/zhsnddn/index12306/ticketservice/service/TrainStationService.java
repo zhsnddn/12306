9f0e6c92a1b30de3be1bfb9f1b3cb219db173f96
朱玉明 <2027606134@qq.com>
2025-08-15 20:10:42 +0800

    feat: 票务系统base部分

--- services/ticket-service/src/main/java/com/zhsnddn/index12306/ticketservice/service/TrainStationService.java
+++ services/ticket-service/src/main/java/com/zhsnddn/index12306/ticketservice/service/TrainStationService.java
@@ -9,17 +9,31 @@ public interface TrainStationService {
 
     /**
      * 根据列车 ID 查询站点信息
+     *
      * @param trainId 列车 ID
-     * @return 站点信息列表
+     * @return 列车经停站信息
      */
     List<TrainStationQueryRespDTO> listTrainStationQuery(String trainId);
 
     /**
-     * 查询列车路线
-     * @param trainId 列车 ID
-     * @param startStation 出发站点
-     * @param endStation    目的站点
-     * @return 路线信息列表
+     * 计算列车站点路线关系
+     * 获取开始站点和目的站点及中间站点信息
+     *
+     * @param trainId   列车 ID
+     * @param departure 出发站
+     * @param arrival   到达站
+     * @return 列车站点路线关系信息
      */
-    List<RouteDTO> listTrainStationRoute(String trainId, String startStation, String endStation);
+    List<RouteDTO> listTrainStationRoute(String trainId, String departure, String arrival);
+
+    /**
+     * 获取需列车站点扣减路线关系
+     * 获取开始站点和目的站点、中间站点以及关联站点信息
+     *
+     * @param trainId   列车 ID
+     * @param departure 出发站
+     * @param arrival   到达站
+     * @return 需扣减列车站点路线关系信息
+     */
+    List<RouteDTO> listTakeoutTrainStationRoute(String trainId, String departure, String arrival);
 }
